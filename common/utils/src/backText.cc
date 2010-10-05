0dddd3a8dd70a87280ac6ee6e3ea9942fff1b19a
danny <danny@5f4161f8-d2e0-4071-b7a4-fd1397524130>
2010-10-05 21:30:52 +0000

    updated the backText extraction to also take a foreground threshold and I am trying to maskout the front text using morphological/set operators. It will still work with only the background threshold. need to update the script because the input method has changed
    git-svn-id: svn+ssh://halsted.vis.uky.edu/var/lib/svnroot/duplexScan@1470 5f4161f8-d2e0-4071-b7a4-fd1397524130

--- common/utils/src/backText.cc
+++ common/utils/src/backText.cc
@@ -19,52 +19,128 @@ namespace po = boost::program_options;
 
 int main(int argc, char** argv)
 {
-	if(argc != 4 )
-	{
-		cerr<<"\nUsage: "<<argv[0] <<"<document> <mask> <outputfile>";
-		return EXIT_FAILURE;
+	string bmfilename, dfilename, fmfilename, ofilename;
+	int bpp;
+	int flip;
+	bool noFlip =false;
+
+	po::options_description desc("Allowed options");
+	desc.add_options()
+   ("help,h", "help message")
+   ("document,d", po::value<string>(&dfilename),"backlit document")
+   ("background-mask,b", po::value<string>(&bmfilename),"background mask file")
+   ("foreground-mask,f", po::value<string>(&fmfilename),"[foreground mask file]")
+   ("output-file,o", po::value<string>(&ofilename),"output filename (don't include extension)")
+   ("bpp", po::value<int>(&bpp)->default_value(16),"bits per pixel for raw")
+   ("flip", po::value<int>(&flip)->default_value(0),
+	 "flip the final result:\n0 - x-axis\n> 0 - y-axis\n< 0 x/y-axis")
+   ("no-flip", "do not flip the result");
+
+	po::variables_map vm;
+	po::store(po::command_line_parser(argc, argv).options(desc).run(), vm);
+	po::notify(vm);
+	
+	if (vm.count("help")) {
+		 cout << desc << endl;
+		 return EXIT_FAILURE;
 	}
-
-	IplImage *doc=0, *mask=0, *result, *tmp;
-
-	//cerr<< "\nreg ";
-	if(0 == (doc=getIplImageFromRAW(argv[1], true, 16)))
+	
+	if(vm.count("background-mask") == 0)
 	{
-		//cerr<< "\npfm ";
-		if(0 == (doc=getIplImageFromPFM(argv[1])))
+		 cout <<"\nYou have to specify a background mask\n"<< desc << endl;
+		 return EXIT_FAILURE;
+	}
+	
+	if(vm.count("output-file") == 0)
+	{
+		 cout <<"\nYou have to specify an output file \n"<< desc << endl;
+		 return EXIT_FAILURE;
+	}
+	
+	noFlip =  (vm.count("no-flip") > 0);
+		
+
+
+	IplImage *doc=0, *fmask=0, *bmask=0, *result, *tmp;
+
+	if(0 == (doc=getIplImageFromRAW(dfilename, true, bpp)))
+	{
+		if(0 == (doc=getIplImageFromPFM(dfilename)))
 		{
-			//cerr<< "\nraw ";
-			if(0 == (doc=cvLoadImage(argv[1], -1)))
+			if(0 == (doc=cvLoadImage(dfilename.c_str(), -1)))
 			{
-				cerr<<"\nFailed to load image file \""<<argv[1]<<"\"\n";
+				cerr<<"\nFailed to load image file \""<<dfilename<<"\" as the document\n";
 				return EXIT_FAILURE;
 			}
 		}
 	}
 
-	if(0 == (mask=cvLoadImage(argv[2], -1)))
+	if(0 == (bmask=cvLoadImage(bmfilename.c_str(), -1)))
 	{
-		cerr<<"\nFailed to load image file \""<<argv[2]<<"\"\n";
+		cerr<<"\nFailed to load image file \""<<bmfilename<<"\" as the background mask\n";
 		return EXIT_FAILURE;
+	}
+
+	if(vm.count("foreground-mask") != 0)
+	{
+		if(0 == (fmask=cvLoadImage(fmfilename.c_str(), -1)))
+		{
+			cerr<<"\nFailed to load image file \""<<fmfilename<<"\" as the foreground mask\n";
+			return EXIT_FAILURE;
+		}
 	}
 
 	result = cvCloneImage(doc);
 	cvZero(result);
-	cvCopy(doc, result, mask);
-	cvFlip(result, 0, 0);
+	tmp = cvCloneImage(bmask);
+	if(fmask)
+	{
+		IplConvKernel* k =cvCreateStructuringElementEx(3, 3, 1, 1, CV_SHAPE_RECT);
+		IplImage *morph = cvCloneImage(fmask);
+		IplImage *dia = cvCloneImage(fmask);
+		cvMorphologyEx(fmask, morph, 0, k, CV_MOP_OPEN, 4);
+		cvSaveImage("foremopOpen.png", morph);
+		cvDilate(morph, dia, 0, 4); 
+		cvSaveImage("foremopDia.png", dia);
+
+		cvNot(dia,morph);
+
+		cvAnd(bmask, morph, tmp);
+		cvSaveImage("andedMask.png", tmp);
+		cvCopy(doc, result, tmp);
+		
+	}
+	else
+		cvCopy(doc, result, tmp);
+
+	if(!noFlip)
+		cvFlip(result, 0, flip);
 
 	tmp = cvCloneImage(doc);
 
 	if(result->depth > 8 )
 	{
-		writePFM(result, (string(argv[3])+".pfm").c_str());
+		writePFM(result, (ofilename+".pfm").c_str());
 		IplImage *scaled = cvCreateImage(cvSize(doc->width, doc->height), IPL_DEPTH_8U, doc->nChannels);
 		scaled->origin = doc->origin;
 		cvConvertScaleAbs(result, scaled, 255.0);
-		cvSaveImage("resultDisplay.png", scaled);
+		//cvSaveImage("resultDisplay.png", scaled);
 		cvConvertScaleAbs(doc, scaled, 255.0);
-		cvSaveImage("docDisplay.png", scaled);
+		//cvSaveImage("docDisplay.png", scaled);
+		cout<<"\nResult saved as \""<<(ofilename+".pfm")<<"\""<<endl;
 	}
 	else
-		cvSaveImage(argv[3], result);
+	{
+		cvSaveImage((ofilename+".png").c_str(), result);
+		cout<<"\nResult saved as \""<<(ofilename+".png")<<"\""<<endl;
+	}
+
+
+	cvReleaseImage(&result);
+	cvReleaseImage(&doc);
+	cvReleaseImage(&tmp);
+	cvReleaseImage(&fmask);
+	cvReleaseImage(&bmask);
+
+	return EXIT_SUCCESS;
 }
